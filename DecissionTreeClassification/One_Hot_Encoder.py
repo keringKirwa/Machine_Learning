84a707cbd92a7dd18467e5574cecb5d54de82ede
arapkering <kkirwa230@gmail.com>
2023-03-01 10:47:09 +0300

    First commit

--- DecissionTreeClassification/One_Hot_Encoder.py
+++ DecissionTreeClassification/One_Hot_Encoder.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 
 data = {
+    "User_ID": [10, 20, 30, 40, 50, 60, 70],
     'Name': ['Tom', 'Joseph', 'Faith', 'John', 'Brenda', 'Ema-son', 'Ian', ],
     'Gender': ['Male', 'Male', 'Female', 'Male', 'Female', 'Male', 'Male', ],
     'Age': [20, 21, 19, 18, 21, 19, 18],
@@ -11,10 +12,25 @@ data = {
 dataFrame = pd.DataFrame(data)
 
 if __name__ == '__main__':
-    print("Pne hot encoding....")
+
+    # drop redundant columns.in this code , the inplace named variable ensures that the original dataframe is modified,
+    # rather than returning the modified version og the original one .
+
+    cols = ["Gender", "Name"]
+    dataFrame.drop(columns=cols, inplace=True)
+    dataFrame.set_index('User_ID', inplace=True)
+
+    dependent_Variables = dataFrame.iloc[:, :-1]
+    target_Variables = dataFrame.iloc[:, -1]
+    print(dependent_Variables)
+    print(target_Variables)
+
+    # note that in machine larning , the One Hot encoder will generate as many columns as the number  of features in the
+    # Gender column(each  column with the attribute name as in the data above.)
+
+    print("One Hot Encoding....")
     gender_encoder = OneHotEncoder()
     gender_encoded = gender_encoder.fit_transform(dataFrame[['Gender']])
     gender_encoded_df = pd.DataFrame(gender_encoded.toarray(), columns=['Female', 'Male'])
     X = pd.concat([dataFrame.drop(['Gender'], axis=1), gender_encoded_df], axis=1)
     print(X)
-
---
+++ Recommendation_System/Movie_Recommendation.py
@@ -0,0 +1,66 @@
+import numpy as np
+from keras.layers import Input, Embedding, Flatten, Dot, Concatenate, Dense
+from keras.models import Model
+from keras.optimizers import Adam
+
+num_users = 50
+num_movies = 100
+
+embedding_size = 32
+
+# Create input layers for user and movie IDs.The input layer of the neaural network accepts an array/tensor with one
+# dimension , and one element at a time eg [10],then  [20] ...
+
+user_input = Input(shape=(1,))
+movie_input = Input(shape=(1,))
+
+age_input = Input(shape=(1,))
+gender_input = Input(shape=(1,))
+
+user_embedding = Embedding(input_dim=num_users, output_dim=embedding_size)(user_input)
+movie_embedding = Embedding(input_dim=num_movies, output_dim=embedding_size)(movie_input)
+
+age_embedding = Embedding(input_dim=100, output_dim=embedding_size)(age_input)
+gender_embedding = Embedding(input_dim=2, output_dim=embedding_size)(gender_input)
+
+
+user_embedding = Flatten()(user_embedding)
+movie_embedding = Flatten()(movie_embedding)
+
+age_embedding = Flatten()(age_embedding)
+gender_embedding = Flatten()(gender_embedding)
+
+user_embedding = Concatenate()([user_embedding, age_embedding, gender_embedding])
+movie_embedding = Concatenate()([movie_embedding])
+
+rating = Dot(axes=-1)([user_embedding, movie_embedding])
+
+# Combine all inputs and outputs into a single model
+model = Model(inputs=[user_input, movie_input, age_input, gender_input], outputs=rating)
+
+# Compile the model with a mean squared error loss function and an Adam optimizer
+model.compile(loss='mse', optimizer=Adam(lr=0.001))
+
+"""Generate some example data"""
+user_ids = np.random.randint(num_users, size=30)
+movie_ids = np.random.randint(num_movies, size=30)
+ages = np.random.randint(18, 60, size=30)
+genders = np.random.randint(2, size=30)
+ratings = np.random.randint(1, 6, size=30)
+
+# Fit the model on the example data
+model.fit([user_ids, movie_ids, ages, genders], ratings, epochs=10, batch_size=10, verbose=2)
+print(model.predict())
+
+if __name__ == '__main__':
+
+    test_users = [24, 30, 45, 50]
+    test_movies = [90, 88, 67, 10]
+    ratings = model.predict([test_users, test_movies])
+    print(ratings)
+
+    for i in range(4):
+        print("User {} is predicted to rate movie {} as {:.2f} out of 5".format(
+            test_users[i], test_movies[i], ratings[i]))
+
+# Print the predicted ratings
